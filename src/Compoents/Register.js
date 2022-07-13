6d5eb9f2453f1922c497780a72b2bda91d81cac2
git config --global user.name <jayshri.pardeshi88@gmail.com>
2022-07-13 16:15:57 +0530

    Register page created

--- src/Compoents/Register.js
+++ src/Compoents/Register.js
@@ -1,11 +1,17 @@
-import React, { Component } from 'react'
+import React, { Component } from "react";
 
 export default class Register extends Component {
   render() {
     return (
-      <div>
-        <h1>Register</h1>
+      <div class="row mt-5">
+        <div class="col-md-6 m-auto">
+          <div class="card card-body">
+            <h1 class="text-center mb-3">
+              <i class="fas fa-user-plus"></i> Register
+            </h1>
+          </div>
+        </div>
       </div>
-    )
+    );
   }
 }
