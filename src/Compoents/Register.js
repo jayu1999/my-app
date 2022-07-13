77290903b2fe6e29ffc0395080f5d1dfe8c9709f
git config --global user.name <jayshri.pardeshi88@gmail.com>
2022-07-13 19:35:45 +0530

    pass and signUp button add

--- src/Compoents/Register.js
+++ src/Compoents/Register.js
@@ -3,21 +3,23 @@ import React, { Component } from "react";
 export default class Register extends Component {
   render() {
     return (
-        <form>
-        <h3>Sign Up</h3>
-        <div className="w-50 p-3 mx-auto ">
-          <label style={{marginBottom:"0.5em"}}>First name</label>
+      <form>
+        <h3 style={{ paddingLeft: "7em", marginTop: "1em" }}>Register </h3>
+        <div className="w-50 p-3">
+          <label>First name</label>
           <input
             type="text"
             className="form-control"
             placeholder="First name"
           />
-          </div>
-        <div className="w-50 p-3 mx-auto">
+        </div>
+
+        <div className="w-50 p-3">
           <label>Last name</label>
           <input type="text" className="form-control" placeholder="Last name" />
         </div>
-        <div className="w-50 p-3 mx-auto">
+
+        <div className="w-50 p-3">
           <label>Email address</label>
           <input
             type="email"
@@ -25,7 +27,25 @@ export default class Register extends Component {
             placeholder="Enter email"
           />
         </div>
-          </form>
+
+        <div className="w-50 p-3">
+          <label>Password</label>
+          <input
+            style={{ marginBottom: "0.9em" }}
+            type="password"
+            className="form-control"
+            placeholder="Enter password"
+          />
+          <div className="d-grid">
+            <button type="submit" className="btn btn-primary">
+              Sign Up{" "}
+            </button>
+          </div>
+          <p className="forgot-password text-right ">
+            Already registered <a href="/sign-in">sign in?</a>
+          </p>
+        </div>
+      </form>
     );
   }
 }
