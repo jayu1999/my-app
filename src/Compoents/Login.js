03a57ff321e351ddb95f70a7e0cb80faf4c507b7
git config --global user.name <jayshri.pardeshi88@gmail.com>
2022-07-13 15:19:11 +0530

    email text box

--- src/Compoents/Login.js
+++ src/Compoents/Login.js
@@ -3,8 +3,27 @@ import React, { Component } from "react";
 export default class Login extends Component {
   render() {
     return (
-      <div>
-        <h1> Login </h1>
+      <div class="row mt-5">
+        <div class="col-md-6 m-auto">
+          <div class="card card-body">
+            <h1 class="text-center mb-3">
+              <i class="fas fa-sign-in-alt"></i> Login
+            </h1>
+
+            <form action="/users/login" method="POST">
+              <div class="form-group">
+                <label for="email">Email</label>
+                <input
+                  type="email"
+                  id="email"
+                  name="email"
+                  class="form-control"
+                  placeholder="Enter Email"
+                />
+              </div>
+            </form>
+          </div>
+        </div>
       </div>
     );
   }
