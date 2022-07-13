e7ffed9d693dcba9c45c99723bc5c70f929c71be
git config --global user.name <jayshri.pardeshi88@gmail.com>
2022-07-13 16:32:38 +0530

    register name box

--- src/Compoents/Register.js
+++ src/Compoents/Register.js
@@ -7,9 +7,21 @@ export default class Register extends Component {
         <div class="col-md-6 m-auto">
           <div class="card card-body">
             <h1 class="text-center mb-3">
-              <i class="fas fa-user-plus"></i> Register
-            </h1>
+              <i class="fas fa-user-plus"></i> Register </h1>
+               
+        <form action="/users/register" method="POST">
+        <div class="form-group">
+          <label for="name">Name</label>
+          <input
+            type="name"
+            id="name"
+            name="name"
+            class="form-control"
+            placeholder="Enter Name"
+            />
           </div>
+        </form>
+        </div>
         </div>
       </div>
     );
