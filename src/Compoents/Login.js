f058093e1ca77856773c5ace09988a7b2ad4eb22
git config --global user.name <jayshri.pardeshi88@gmail.com>
2022-07-15 08:52:09 +0530

    constructor and handlesubmit method use

--- src/Compoents/Login.js
+++ src/Compoents/Login.js
@@ -1,6 +1,42 @@
 import React, { Component } from "react";
 
 export default class Login extends Component {
+  constructor(props){
+    super(props);
+    this.state ={
+      email:"",
+      password:"",
+    };
+    this.handleSubmit = this.handleSubmit.bind(this);
+  }
+  handleSubmit(e){
+    e.preventDefault();
+    const { email,password }=this.state;
+    console.log(email,password);
+    fetch("http://localhost:5000/login-user",{
+    method:"POST",
+    crossDomain:True;
+    headers:{
+      "Content-Type":"application/json",
+      Accept:"application/json",
+      "Access-Control-Allow-Origin":"*",
+    },
+    body:JSON.stringify({
+      email,
+      password,
+    }),
+   })
+    .then((res)=> res.json())
+    .then((data) =>{
+      console.log(data ,"userRegister");
+      if(data.status == 'ok'){
+        alert("login successful")
+        window.localStoarge.setItem("token",data.data);
+        window.location.href ="./userDetails";
+      }
+    });
+
+  }
   render() {
     return (
       <div className="row mt-5">
@@ -19,9 +55,11 @@ export default class Login extends Component {
                   name="email"
                   className="form-control"
                   placeholder="Enter Email"
+                  onChange={(e) => this.setState({password:e.target.value})}
+
                 />
               </div>
-              <div className="form-group"  style={{ marginTop: "0.7em" }}>
+              <div className="form-group" style={{ marginTop: "0.7em" }}>
                 <label htmlFor="password">Password</label>
                 <input
                   type="password"
@@ -29,13 +67,21 @@ export default class Login extends Component {
                   name="password"
                   className="form-control"
                   placeholder="Enter Password"
+                  onChange={(e) => this.setState({password:e.target.value})}
+
                 />
               </div>
-              <button type="submit" className="btn btn-primary btn-block"  style={{ marginTop: "0.7em" }}>
+              <button
+                type="submit"
+                className="btn btn-primary btn-block"
+                style={{ marginTop: "0.7em" }}
+              >
                 Login{" "}
               </button>
             </form>
-            <p className="forgot-password text-right"  style={{ marginTop: "0.9em" }}>
+            <p
+              className="forgot-password text-right"
+              style={{ marginTop: "0.9em" }}>
               register here <a href="/Register">sign up?</a>
             </p>
           </div>
